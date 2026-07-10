b9f926a9ed0eb869807f39398d528fd2d7e75790
fefarpari <fefarpari67@gmail.com>
2026-07-10 09:19:16 +0530

    Add files via upload

---
+++ np_array.py
@@ -0,0 +1,16 @@
+import sys
+import numpy as np
+arr1=np.array([10,20,30,40,50])
+print(arr1)
+
+arr2=np.array([20,40,60,80,100])
+print(arr2)
+
+print(arr1+arr2)
+print(arr1-arr2)
+print(arr1*arr2)
+print(arr1/arr2)
+print(arr1+50)
+print(arr1-70)
+print(arr1*60)
+print(arr1/30)

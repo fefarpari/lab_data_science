96ba026ad90a53cd1fcb7128b51aea65d25545c7
fefarpari <fefarpari67@gmail.com>
2026-07-14 08:29:19 +0530

    Add files via upload

---
+++ np_multi_array.py
@@ -0,0 +1,15 @@
+import sys
+import numpy as np
+
+arr=np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print(arr)
+print(arr[0,0])
+print(arr[0,1])
+print(arr[0,2])
+print(arr[0,3])
+
+print(arr[1,0])
+print(arr[1,1])
+print(arr[1,2])
+print(arr[1,3])
+print(arr[1,4])

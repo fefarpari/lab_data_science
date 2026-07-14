d6815c081f633b28ad948ba6a4e0c33876fad449
fefarpari <fefarpari67@gmail.com>
2026-07-14 08:28:56 +0530

    Add files via upload

---
+++ np_fun.py
@@ -0,0 +1,20 @@
+import sys 
+import numpy as np
+arr=np.array([1,4,9,16,25])
+print(arr)
+
+print("sqrt : ",np.sqrt(arr))
+print("square : ",np.square(arr))
+print("sin : ",np.sin(arr))
+print("cos : ",np.cos(arr))
+
+import sys
+import numpy as np
+arr1=np.array([10,20,30,40,50])
+print(arr1)
+
+print("sum :",np.sum(arr1))
+print("min : ",np.min(arr1))
+print("max :",np.max(arr1))
+print("mean :",np.mean(arr1))
+print("std :",np.std(arr1))

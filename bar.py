d77bc1942725c77a9d873b2ea947c9e47ce51140
fefarpari <fefarpari67@gmail.com>
2026-07-31 09:12:47 +0530

    Add files via upload

---
+++ bar.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+df=pd.read_csv(r"D:\E2_10\e2_ds_10\sales_data.csv")
+plt.bar(df["Month"],df["Sales"],color="#246789")
+plt.title("Monthly Sales")
+plt.xlabel("Month")
+plt.ylabel("Sales")
+plt.show()
